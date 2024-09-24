8ff19e7dd47274b96a5cf0b2a764802343048b62
llamouth <150949923+llamouth@users.noreply.github.com>
2024-09-24 15:42:18 -0400

    feat: added app.js from test

--- App.js
+++ App.js
@@ -1,30 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import SignUp from "./Screens/SignUp"
-
-const Stack = createNativeStackNavigator();
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './Screens/Login';
+import Home from './Screens/Home';
+import Profile from './Screens/Profile';
+import SignUp from './Screens/SignUp';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
