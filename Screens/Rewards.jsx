2ca6592ebc8abb22f563bc0e020a4a25ef5c3f9d
llamouth <150949923+llamouth@users.noreply.github.com>
2024-10-07 18:04:15 -0400

    add necessary repos

---
+++ Screens/Rewards.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Rewards = () => {
+    return (
+        <View>
+            <Text>Rewards</Text>
+        </View>
+    )
+}
+
+export default Rewards
